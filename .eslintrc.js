99105741ff54274e2e7431b3df779403e40425cb
马浩 <303527871@qq.com>
2024-06-24 16:31:19 +0800

    fix:eslint&prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
   root: true,
-  extends: ["universe/native"],
+  extends: ["universe/native", 'expo', "prettier"],
+  plugins: ['prettier'],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "off",
+    'prettier/prettier': 'warn',
   },
 };
