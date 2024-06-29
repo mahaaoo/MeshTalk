13760953e7bf3e21137e18d1e42aba04ebabdced
马浩 <303527871@qq.com>
2024-06-30 00:01:08 +0800

    fix:loading use DefaultUser

--- src/app/user/[id].tsx
+++ src/app/user/[id].tsx
@@ -3,7 +3,6 @@ import User from "@ui/user";
 import DefaultUser from "@ui/user/defaultUser";
 import { useLocalSearchParams } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
 
 import { Account } from "../../config/interface";
 import { lookupAcct } from "../../server/account";
@@ -27,19 +26,10 @@ const Users: React.FC<object> = () => {
   return (
     <Screen>
       {
-        // !userData ? <View style={styles.main} /> : <User userData={userData} id={userData.id} />
+        !userData ? <DefaultUser /> : <User userData={userData} id={userData.id} />
       }
-      <DefaultUser />
     </Screen>
   );
 };
-
-const styles = StyleSheet.create({
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  }
-})
 
 export default Users;
