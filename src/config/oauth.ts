046b45254bc8c23852c95553ba5a8e33dc9d5392
马浩 <303527871@qq.com>
2024-07-02 16:28:14 +0800

    feat:publish to github page

--- src/config/oauth.ts
+++ src/config/oauth.ts
@@ -1,5 +1,5 @@
 const ClientName: string = "MeshTalk";
-const RedirectUris: string = "https://github.com/mahaaoo";
+const RedirectUris: string = "https://mahaaoo.github.io/MeshTalk";
 // const RedirectUris: string = "http://localhost:8081/login";
 const Scopes: string = "read write follow push";
 
