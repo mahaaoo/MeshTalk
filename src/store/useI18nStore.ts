ce0e3772b89bcfcda60a1f28213f2139d11a335c
马浩 <303527871@qq.com>
2024-07-24 22:44:16 +0800

    feat:move local from i18 to preference 2

--- src/store/useI18nStore.ts
+++ src/store/useI18nStore.ts
@@ -1,43 +1,15 @@
-import { getLocales } from "expo-localization";
 import { I18n } from "i18n-js";
 import { create } from "zustand";
 
 import { SupportLocaleProps, translations, support } from "../../locales";
 interface I18nState {
   i18n: I18n;
-  initI18n: () => void;
-  local: SupportLocaleProps | undefined;
   switchLocale: (local: SupportLocaleProps) => void;
   getSupportLocale: () => SupportLocaleProps[];
 }
 
 const useI18nStore = create<I18nState>((set, get) => ({
   i18n: new I18n(translations),
-  local: undefined,
-  initI18n: () => {
-    const i18n = get().i18n;
-    let languageCode = getLocales()[0].languageCode ?? "en";
-    let local;
-
-    if (support.map((sup) => sup.locale).indexOf(languageCode) === -1) {
-      // app不支持当前地区语言，默认en
-      languageCode = "en";
-    }
-
-    i18n.locale = languageCode;
-
-    support.forEach((sup) => {
-      if (sup.locale === languageCode) {
-        local = { ...sup };
-      }
-    });
-
-    i18n.enableFallback = true;
-    set({
-      i18n,
-      local,
-    });
-  },
   getSupportLocale: () => {
     return support;
   },
@@ -47,7 +19,6 @@ const useI18nStore = create<I18nState>((set, get) => ({
 
     set({
       i18n,
-      local,
     });
   },
 }));
--- src/store/usePreferenceStore.ts
+++ src/store/usePreferenceStore.ts
@@ -6,6 +6,7 @@ import { SupportLocaleProps, translations, support } from "../../locales";
 import { getLocales } from "expo-localization";
 import { getItem } from "@utils/storage";
 import { PREFERENCES } from "../config/constant";
+import useI18nStore from "./useI18nStore";
 
 interface PreferenceStorage {
   local: SupportLocaleProps;
@@ -25,6 +26,26 @@ interface PreferenceStoreState {
   initPreference: () => void;
 }
 
+const initLocale = () => {
+  let languageCode = getLocales()[0].languageCode ?? "en";
+  let local;
+
+  if (support.map((sup) => sup.locale).indexOf(languageCode) === -1) {
+    // app不支持当前地区语言，默认en
+    languageCode = "en";
+  }
+
+  support.forEach((sup) => {
+    if (sup.locale === languageCode) {
+      local = { ...sup };
+    }
+  });
+  return {
+    local,
+    languageCode,
+  };
+};
+
 const usePreferenceStore = create<PreferenceStoreState>((set, get) => ({
   local: undefined,
   sensitive: false,
@@ -39,34 +60,33 @@ const usePreferenceStore = create<PreferenceStoreState>((set, get) => ({
   initPreference: async () => {
     const i18n = new I18n(translations);
     i18n.enableFallback = true;
-
+    let initLocal;
     const preferenceJson = await getItem(PREFERENCES);
     if (preferenceJson) {
       const preference = JSON.parse(preferenceJson) as PreferenceStorage;
 
       if (preference.local) {
         i18n.locale = preference.local.locale;
+        initLocal = preference.local;
       } else {
         // 初始化偏好设置
-        let languageCode = getLocales()[0].languageCode ?? "en";
-        let local;
-
-        if (support.map((sup) => sup.locale).indexOf(languageCode) === -1) {
-          // app不支持当前地区语言，默认en
-          languageCode = "en";
-        }
-
+        const { languageCode, local } = initLocale();
         i18n.locale = languageCode;
-
-        support.forEach((sup) => {
-          if (sup.locale === languageCode) {
-            local = { ...sup };
-          }
-        });
+        initLocal = local;
       }
-
+    } else {
+      const { languageCode, local } = initLocale();
+      i18n.locale = languageCode;
+      initLocal = local;
     }
-  }
+
+    useI18nStore.setState({
+      i18n,
+    });
+    set({
+      local: initLocal,
+    });
+  },
 }));
 
 export default usePreferenceStore;
